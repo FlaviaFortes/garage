54251491658dd259e071e4cd1104e97c75014226
Flavia Fortes <flaviafortes88@gmail.com>
2013-10-19 03:03:06 -0300

    Create migration that adds image columns to Photos

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131019053458) do
+ActiveRecord::Schema.define(version: 20131019060140) do
 
   create_table "artists", force: true do |t|
     t.string   "name"
@@ -33,6 +33,10 @@ ActiveRecord::Schema.define(version: 20131019053458) do
     t.integer  "artist_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "image_file_name"
+    t.string   "image_content_type"
+    t.integer  "image_file_size"
+    t.datetime "image_updated_at"
   end
 
   add_index "photos", ["artist_id"], name: "index_photos_on_artist_id", using: :btree
