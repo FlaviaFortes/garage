cb166be8c9b26b737d6aca25267561da27a0af18
Flavia Fortes <flaviafortes88@gmail.com>
2013-10-19 15:11:39 -0300

    Implement Artist#create

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -7,4 +7,19 @@ class ArtistsController < ApplicationController
   def new
     @artist = Artist.new
   end
+
+  def create
+    @artist = Artist.new(artist_params)
+    # binding.pry
+    if @artist.save
+      redirect_to artists_path, notice: "Perfil criado com sucesso! Seja bem-vindo ao Garage!"
+    else
+      render 'new'
+    end
+  end
+
+ private
+    def artist_params
+      params.require(:artist).permit(:name, :email, :description, :site, :avatar)
+    end
 end
--- spec/controllers/artists_controller_spec.rb
+++ spec/controllers/artists_controller_spec.rb
@@ -1,9 +1,9 @@
 require 'spec_helper'
 
 describe ArtistsController do
-  let!(:artist) { create(:artist) }
 
   describe "GET 'index'" do
+    let!(:artist) { create(:artist) }
 
     before do
       get :index
@@ -30,6 +30,65 @@ describe ArtistsController do
 
     it "should assign a new post" do
       expect(assigns(:artist)).to be_a_new(Artist)
+    end
+  end
+
+  describe "POST 'create'" do
+
+    context "with valid params" do
+
+      let(:params) do
+        { artist:  attributes_for(:artist) }
+      end
+
+      it "create a new artist" do
+        expect do
+          post :create, params
+        end.to change(Artist, :count).by(1)
+      end
+
+      it "assign a newly @artist" do
+        post :create, params
+        expect(assigns(:artist)).to be_a(Artist)
+      end
+
+      it "@artist is persisted" do
+        post :create, params
+        expect(assigns(:artist)).to be_persisted
+      end
+
+      it "redirect to the artists_path" do
+        post :create, params
+        should redirect_to(artists_path)
+      end
+
+      it "set the flash correctly" do
+        post :create, params
+        should set_the_flash[:notice].to(/sucesso/)
+      end
+    end
+
+    context "with invalid params" do
+
+      let(:params) do
+        { artist:  attributes_for(:artist, name: "") }
+      end
+
+      it "does not create a new artist" do
+        expect do
+          post :create, params
+        end.to_not change(Artist, :count)
+      end
+
+      it "does not assign @artist with a new one" do
+        post :create, params
+        expect(assigns(:artist)).to be_a_new(Artist)
+      end
+
+      it "re-render the 'new' template" do
+          post :create, params
+          should render_template("new")
+      end
     end
   end
 end
