d593ea1cf086013d74df00276f28b7c2166ee943
Flavia Fortes <flaviafortes88@gmail.com>
2013-10-19 14:19:26 -0300

    Fix Artist factory

--- spec/factories/artists.rb
+++ spec/factories/artists.rb
@@ -5,10 +5,10 @@ FactoryGirl.define do
     email "taylorswift@gmail.com"
 
     before :create do |artist|
-      artist.image_file_name= "picture.jpg"
-      artist.image_content_type = "image/jpeg"
-      artist.image_file_size = 2588
-      artist.image_updated_at = Time.zone.now
+      artist.avatar_file_name= "picture.jpg"
+      artist.avatar_content_type = "image/jpeg"
+      artist.avatar_file_size = 2588
+      artist.avatar_updated_at = Time.zone.now
     end
   end
 end
