f474c0d3799a2460c21deba2f08fb1974202c6e8
Israel Ribeiro <israveri@gmail.com>
2013-10-20 02:33:20 -0200

    Fixes paperclip storage config

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -1,6 +1,6 @@
 if Rails.env.production? or Rails.env.staging?
-  Paperclip::Attachment.default_options[:storage] = :file
-  Paperclip::Attachment.default_options[:url] = ":file_domain_url"
+  Paperclip::Attachment.default_options[:storage] = :filesystem
+  Paperclip::Attachment.default_options[:url] = "/system/:class/:attachment/:id_partition/:style/:filename"
 end
 
 Paperclip::Attachment.default_options[:hash_secret] = ENV['PAPERCLIP_SECRET']
