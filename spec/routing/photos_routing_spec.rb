f9fbe8dc24979a99f40727569e5d86ae759bf854
Flavia Fortes <flaviafortes88@gmail.com>
2013-10-20 01:26:17 -0300

    Add test for Photos#new

--- spec/routing/photos_routing_spec.rb
+++ spec/routing/photos_routing_spec.rb
@@ -6,12 +6,18 @@ describe ArtistsController do
     it "routes to #index" do
       expect(get("/artists/1/photos")).to route_to("photos#index", artist_id: "1")
     end
+    it "routes to #new" do
+      expect(get("/artists/1/photos/new")).to route_to("photos#new", artist_id: "1")
+    end
   end
 
   describe "route helpers" do
 
     it "artist_photos_path" do
       expect(artist_photos_path(1)).to eq("/artists/1/photos")
+    end
+    it "new_artist_photo_path" do
+      expect(new_artist_photo_path(1)).to eq("/artists/1/photos/new")
     end
   end
 end
