720f820d03c6da1308d5c8370b5af97abbb0a439
Flavia Fortes <flaviafortes88@gmail.com>
2013-10-20 12:53:17 -0300

    Implement Photos#create

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -9,4 +9,19 @@ class PhotosController < ApplicationController
     @artist = Artist.find(params[:artist_id])
     @photo = Photo.new(artist: @artist)
   end
+
+  def create
+    @artist = Artist.find(params[:artist_id])
+    @photo = @artist.photos.new(photo_params)
+    if @photo.save
+      redirect_to artist_path(@artist)
+    else
+      render 'new'
+    end
+  end
+
+  private
+    def photo_params
+      params.require(:photo).permit(:image, :description)
+    end
 end
--- spec/controllers/photo_controller_spec.rb
+++ spec/controllers/photo_controller_spec.rb
@@ -27,7 +27,7 @@ describe PhotosController do
     let(:artist) { create(:artist) }
     let(:photo) { create(:photo, artist: artist) }
 
-    context "request" do
+    context "with valid artist_id" do
       before do
         get :new, artist_id: artist.id
       end
@@ -36,12 +36,67 @@ describe PhotosController do
       it { should render_template(:new) }
       it { should render_with_layout(:application) }
 
-      it "assign correct artist" do
-        expect(assigns(:artist)).to eq(artist)
-      end
-
       it "assign a new photo" do
         expect(assigns(:photo)).to be_a_new(Photo)
+      end
+    end
+
+    context "with invalid artist_id" do
+
+      it "should raise error" do
+        expect do
+          get :new, artist_id: 'wrong'
+        end.to raise_error(ActiveRecord::RecordNotFound)
+      end
+    end
+  end
+
+  describe "POST 'create'" do
+    let(:artist) { create(:artist) }
+
+    context "with valid artist_id" do
+
+      let(:params) do
+        { photo:  attributes_for(:photo),
+          artist_id: artist.id }
+      end
+
+      it "create a new photo" do
+        expect do
+          post :create, params
+        end.to change(Photo, :count).by(1)
+      end
+
+      it "assign a newly @photo" do
+        post :create, params
+        expect(assigns(:photo)).to be_a(Photo)
+      end
+
+      it "@photo is persisted" do
+        post :create, params
+        expect(assigns(:photo)).to be_persisted
+      end
+
+      it "create a photo for the right artist" do
+        expect do
+          post :create, params
+        end.to change{ artist.photos.count }.by(1)
+      end
+
+      it "redirect to the artist_path" do
+        post :create, params
+        should redirect_to(artist_path(artist))
+      end
+    end
+
+    #TODO Create Tests For Invalid Params (image = nil)
+
+    context "with invalid artist id" do
+
+      it "should raise error" do
+        expect do
+          post :create, artist_id: 'wrong'
+        end.to raise_error(ActiveRecord::RecordNotFound)
       end
     end
   end
