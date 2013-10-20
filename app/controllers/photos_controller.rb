7cd4fde5082c4baf7aad2c67b66e71ff29f20ebe
Flavia Fortes <flaviafortes88@gmail.com>
2013-10-20 01:44:39 -0300

    Implement Photos#new

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -4,4 +4,9 @@ class PhotosController < ApplicationController
     @artist = Artist.find(params[:artist_id])
     @photos = Photo.where(artist: @artist)
   end
+
+  def new
+    @artist = Artist.find(params[:artist_id])
+    @photo = Photo.new(artist: @artist)
+  end
 end
--- spec/controllers/photo_controller_spec.rb
+++ spec/controllers/photo_controller_spec.rb
@@ -22,4 +22,27 @@ describe PhotosController do
       expect(assigns(:photos)).to include(photo)
     end
   end
+
+  describe "GET 'new'" do
+    let(:artist) { create(:artist) }
+    let(:photo) { create(:photo, artist: artist) }
+
+    context "request" do
+      before do
+        get :new, artist_id: artist.id
+      end
+
+      it { should respond_with(:success) }
+      it { should render_template(:new) }
+      it { should render_with_layout(:application) }
+
+      it "assign correct artist" do
+        expect(assigns(:artist)).to eq(artist)
+      end
+
+      it "assign a new photo" do
+        expect(assigns(:photo)).to be_a_new(Photo)
+      end
+    end
+  end
 end
